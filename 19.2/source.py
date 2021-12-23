bf6b0a0a65ac9d03081520eae591a9f0ef5ef541
BramFam96 <mtbramlett25@gmail.com>
2021-12-23 13:30:11 -0600

    renamed config

---
+++ 19.2/source.py
@@ -0,0 +1,12 @@
+# Import flask
+# Include render_template to serve files:
+from flask import Flask, request, render_template
+# Import debug toolbar:
+from flask_debugtoolbar import DebugToolbarExtension;
+
+app = Flask(__name__);
+
+# Config and init toolbar:
+app.config['SECRET_KEY'] = 'something';
+# TODO Env variables
+debug = DebugToolbarExtension(app);
