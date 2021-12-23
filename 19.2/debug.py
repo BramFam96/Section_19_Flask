8d219f466a2ad90d2648d1f6fc6129ecf7701cef
BramFam96 <mtbramlett25@gmail.com>
2021-12-23 13:30:46 -0600

    notes on initialization

---
+++ 19.2/debug.py
@@ -0,0 +1,16 @@
+# Imports:
+from source import *
+###################NOTE MOVED to source.py NOTE###################
+
+#  Initialize toolbar:
+
+# from flask_debugtoolbar import DebugToolbarExtension;
+
+# app.config['SECRET_KEY'] = 'something';
+# TODO env vars and debug sessions  #
+
+# debug = DebugToolbarExtension(app);
+
+###################NOTE MOVED to source.py NOTE###################
+
+
