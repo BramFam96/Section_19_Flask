eeca2e9b43ba53fc06a82bccb22bc84e4f3d082d
BramFam96 <mtbramlett25@gmail.com>
2022-01-06 13:00:55 -0600

    reformatted base

--- 19.2_Jinja/static_files.py
+++ 19.2_Jinja/static_files.py
@@ -1,2 +1,7 @@
 # Jinja can be used to serve static files, such as JS and CSS;
-# To use them we create a static folder:
+# To do this, simply link to them in individual templates.
+
+# In practice, we would link the majority of code in a base template:
+    # Extend this template:
+      # then link new files only for specific style/behavior.
+
